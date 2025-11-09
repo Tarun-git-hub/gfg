c85f08e6711d9b558b45f95057fa015f7b73c08f
Tarun-git-hub <t.kumar05092006@gmail.com>
2025-11-09 12:21:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.cpp
@@ -0,0 +1,17 @@
+// User function Template for C++
+
+
+void fizzBuzz(int n) {
+    // Write your code here.
+    if(n%3==0 && n%5==0)
+    cout<<"FizzBuzz"<<endl;
+    
+    else if(n%3==0)
+    cout<<"Fizz"<<endl;
+    
+    else if(n%5==0)
+    cout<<"Buzz"<<endl;
+    
+    else 
+    cout<<n<<endl;
+}
