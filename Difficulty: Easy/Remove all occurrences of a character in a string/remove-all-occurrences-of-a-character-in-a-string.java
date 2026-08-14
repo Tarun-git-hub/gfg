afed0daf8bf2079fad1b42fc2a49af5ef5f445db
Tarun-git-hub <t.kumar05092006@gmail.com>
2026-08-14 12:44:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove all occurrences of a character in a string/remove-all-occurrences-of-a-character-in-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    // Function to remove all occurrences of the character from the string
+    public void removeCharacter(StringBuilder s, char c) {
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == c) {
+                s.deleteCharAt(i);
+                i--;  // check the new character at this index
+            }
+        }
+    }
+}
